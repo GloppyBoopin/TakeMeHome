5fbf96c8b0f5d52885fc5d5d4b2aaa8455170c1c
Balandin Andrey <ABalandin@beeline.kz>
2021-01-22 11:04:10 +0600

    prototype

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TakeMeHome - главная</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<header></header>
+	<main>
+		<div class="slider"></div>
+		<div class="site-nav"></div>
+		<div class="shop-nav">
+			<div class="filler-nav">
+				<div class="filler-nav-item"></div>
+				<div class="filler-nav-item"></div>
+				<div class="filler-nav-item"></div>
+			</div>
+			<div class="delivery-promotion"></div>
+			<div class="categories-nav">
+				<div class="categories-nav-item"></div>
+				<div class="categories-nav-item"></div>
+				<div class="categories-nav-item"></div>
+				<div class="categories-nav-item"></div>
+			</div>
+		</div>
+	</main>
+	<footer></footer>
+</body>
+</html>
