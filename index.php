d1223f668b43d429ed6bde0095b713ab19f92a6f
GloppyBoopin <coucage@gmail.com>
2021-01-24 17:59:30 +0600

    main page stuff

--- index.php
+++ index.php
@@ -3,12 +3,56 @@
 <head>
 	<title>TakeMeHome - главная</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-	<header></header>
+	<header>
+		<div class="header-nav">
+			<a href="" class="home-link">
+				<img src="images/logo-1.png" class="logo" alt="logo">
+				<span class="name"><b>TakeMeHome</b></span>
+			</a>
+			<div class="header-phone-numbers">
+				+7(800)555-35-35
+				<br>
+				+7(777)221-66-66
+			</div>
+			<div class="header-nav-form-link-ad">
+				<!-- search form -->
+				<form>  
+					<input class="search" type="text" name="">
+					<button class="search-button"><i class="fa fa-search"></i></button>
+				</form>
+				<a href="" class="link-ad">ПОДАТЬ ОБЪЯВЛЕНИЕ</a>
+			</div>
+		</div>
+	</header>
 	<main>
 		<div class="slider"></div>
-		<div class="site-nav"></div>
+		<div class="site-nav">
+			<div class="site-nav-link-container">
+				<!-- shop link -->
+				<a href="" class="site-nav-link">
+					<img src="images/shopping-bag.png" class="site-nav-image">
+					<p class="site-nav-p">МАГАЗИН</p>
+				</a>
+				<!-- free ads link -->
+				<a href="" class="site-nav-link">
+					<img src="images/home.png" class="site-nav-image">
+					<p class="site-nav-p">ДАРОМ</p>
+				</a>
+				<!-- ads link -->
+				<a href="" class="site-nav-link">
+					<img src="images/pawprint.png" class="site-nav-image">
+					<p class="site-nav-p">ПРОДАЖА</p>
+				</a>
+				<!-- lost and found link -->
+				<a href="" class="site-nav-link">
+					<img src="images/pet-house.png" class="site-nav-image">
+					<p class="site-nav-p" style="margin: 5px -25px;">БЮРО НАХОДОК</p>
+				</a>
+			</div>
+		</div>
 		<div class="shop-nav">
 			<div class="filler-nav">
 				<div class="filler-nav-item"></div>
