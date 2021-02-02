96408bed505e8b98df53be613b1cdecfff40316a
GloppyBoopin <coucage@gmail.com>
2021-02-02 15:24:04 +0600

    ad desk page - первые наброски

---
+++ addesk.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TakeMeHome - объявления</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/addesk.css">
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico" />
+</head>
+<body>
+	<header>
+		<div class="header-nav">
+			<a href="" class="home-link">
+				<img src="images/logo-1.png" class="logo" alt="logo">
+				<span class="name"><b>TakeMeHome</b></span>
+			</a>
+			<div class="header-phone-numbers">
+				+7(800)555-35-35
+				<br>
+				+7(777)221-66-66
+			</div>
+			<div class="header-nav-form-link-ad">
+				<!-- search form -->
+				<!-- <form>  
+					<input class="search" type="text" name="">
+					<button class="search-button"><i class="fa fa-search"></i></button>
+				</form> -->
+			</div>
+		</div>
+		<a href="" class="link-ad-addesk">ПОДАТЬ ОБЪЯВЛЕНИЕ</a>
+	</header>
+</body>
+<main>
+	<div class="search-wrapper">
+		<form>  
+			<input class="search" type="text" name="">
+			<button class="search-button"><i class="fa fa-search"></i></button>
+		</form>
+	</div>
+
+	<div class="addesk">
+		<div class="filters-wrapper">
+			<div class="pet-category-filters">
+				<!-- Нажимая на одну из этих кнопок пользователь отправляется на страницу с соответствующими животными, добавляя туда блок с фильтрами для данной категории животных -->
+				<a href="" class="pet-category-link">
+					<img src="images/dog-minimal.png" alt="" class="pet-category-image">
+					<p>Собаки</p>
+					<img src="images/dropdown.png" alt="" class="dropdown-image">
+				</a>
+				<a href="" class="pet-category-link">
+					<img src="images/cat-minimal.png" alt="" class="pet-category-image">
+					<p>Кошки</p>
+					<img src="images/dropdown.png" alt="" class="dropdown-image">
+				</a>
+				<a href="" class="pet-category-link">
+					<img src="images/hamster-minimal.png" alt="" class="pet-category-image">
+					<p>Грызуны</p>
+					<img src="images/dropdown.png" alt="" class="dropdown-image">
+				</a>
+				<a href="" class="pet-category-link">
+					<img src="images/bird-minimal.png" alt="" class="pet-category-image">
+					<p>Птицы</p>
+					<img src="images/dropdown.png" alt="" class="dropdown-image">
+				</a>
+				<a href="" class="pet-category-link">
+					<!-- <img src="" alt="" class="pet-category-image"> -->
+					<p>Прочее</p>
+					<img src="images/dropdown.png" alt="" class="dropdown-image">
+				</a>
+			</div>
+		</div>
+	</div>
+</main>
+</html>
